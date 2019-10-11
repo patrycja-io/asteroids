69b1891ecf6d83a3f56543d2a1bbfbf95a541a6d
Patrycja <patrycjatyra@protonmail.com>
2019-10-11 10:54:10 +0000

    game loop set up

--- assets/js/astro.js
+++ assets/js/astro.js
@@ -4,4 +4,7 @@ const canvas = document.getElementById("astroCanvas");
 
 // Context from the canvas
 
-const context = canvas.getContext("2d")
+const context = canvas.getContext("2d");
+
+// Game loop set  up
+setInterval(update, 1000 / FPS);
