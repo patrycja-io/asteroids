ffbf1b37adcbe67095bf2124e9ce41a894d4da46
Patrycja <patrycjatyra@protonmail.com>
2019-11-05 22:03:46 +0000

    added option toggle container

--- assets/jquery/hide.js
+++ assets/jquery/hide.js
@@ -1,3 +1,12 @@
-$("show").click(function(){
-    $(".history").toggle();
-  });
+$('.clickButtons').click(function(e) {
+  e.preventDefault();
+  const id = this.id;
+	console.log(this.id);
+  if (id === 'topButton') {
+		    $('#bottomContainer').show();
+    $('#topContainer').hide();
+} else {	
+    $('#topContainer').show();
+    $('#bottomContainer').hide();
+  }
+});
