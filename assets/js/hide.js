21eac5434c38ece9db37e3e695d65c67e59e4687
Patrycja <patrycjatyra@protonmail.com>
2019-11-06 23:15:24 +0000

    updated javascript code for show and hide

--- assets/js/hide.js
+++ assets/js/hide.js
@@ -1,12 +1,13 @@
 $('.clickButtons').click(function(e) {
   e.preventDefault();
   const id = this.id;
-	console.log(this.id);
+    console.log(this.id);
   if (id === 'topButton') {
-		    $('#bottomContainer').show();
-    $('#topContainer').hide();
-} else {	
-    $('#topContainer').show();
-    $('#bottomContainer').hide();
+    $('#game').show();
+    $('#history').hide();
+    $('.footer').remove();
+} else {    
+    $('#history').show();
+    $('#game').hide();
   }
 });
