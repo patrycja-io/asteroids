d75585cd4cb97916f589599a4d052aa5a4be175e
Patrycja <patrycjatyra@protonmail.com>
2019-11-10 13:35:13 +0000

    removed footer from  hide function

--- assets/js/hide.js
+++ assets/js/hide.js
@@ -1,14 +1,11 @@
 $('.clickButtons').click(function(e) {
   e.preventDefault();
   const id = this.id;
-    console.log(this.id);
   if (id === 'topButton') {
-    $('#history').hide(1200);
-    $('footer').hide(1200);
-    $('#game').show(1200);
+    $('#history').hide();
+    $('#game').show();
 } else {    
-    $('#history').show(1200);
-    $('footer').show(1200);
-    $('#game').hide(1200);
+    $('#history').show();
+    $('#game').hide();
   }
 });
